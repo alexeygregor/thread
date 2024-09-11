fc0f012cd1285fe45180a7185a0313722784ac0e
alexeygregor <161445776+alexeygregor@users.noreply.github.com>
2024-09-11 12:26:09 +0600

    Add files via upload

---
+++ station.cpp
@@ -0,0 +1,140 @@
+﻿#include <cassert>
+#include <iostream>
+#include <mutex>
+#include <string>
+#include <thread>
+#include <vector>
+using namespace std;
+
+mutex station;
+mutex depo_access;
+vector<int>depo = {0, 0, 0};
+
+bool TrainA(int train1)
+{
+	this_thread::sleep_for(chrono::seconds(train1));
+	depo_access.lock();
+	for (int i = 0; i < 3; ++i)
+	{
+		if (depo[i] > 0 && i != 0)
+		{
+			cout << "\nTrain A in depo" << endl;
+			break;
+		}
+	}
+	depo[0] = 1;
+	depo_access.unlock();
+	return true;
+}
+
+bool TrainB(int train2)
+{
+	this_thread::sleep_for(chrono::seconds(train2));
+	depo_access.lock();
+	for (int i = 0; i < 3; ++i)
+	{
+		if (depo[i] > 0 && i != 1)
+		{
+			cout << "\nTrain B in depo" << endl;
+			break;
+		}
+	}
+	depo[1] = 2;
+	depo_access.unlock();
+	return true;
+}
+
+bool TrainC(int train3)
+{
+	this_thread::sleep_for(chrono::seconds(train3));
+	depo_access.lock();
+	for (int i = 0; i < 3; ++i)
+	{
+		if (depo[i] > 0 && i != 2)
+		{
+			cout << "\nTrain C in depo" << endl;
+			break;
+		}
+	}
+	depo[2] = 3;
+	depo_access.unlock();
+	return true;
+}
+
+void Admin(int value)
+{
+	string command;
+	int q = 0;
+	while (q != -1)
+	{
+		cin >> command;
+		if (command == "depart")
+		{
+			depo[value] = 0;
+			q--;
+		}
+	}
+}
+
+int Station(int count)
+{
+	station.lock();
+	for (int i = 0; i < 3; ++i)
+	{
+		if (depo[i] == 1)
+		{
+			cout << "\nTrain A in station" << endl;
+			Admin(i);
+			count++;
+			break;
+		}
+		if (depo[i] == 2)
+		{
+			cout << "\nTrain B in station" << endl;
+			Admin(i);
+			count++;
+			break;
+		}
+		if (depo[i] == 3)
+		{
+			cout << "\nTrain C in station" << endl;
+			Admin(i);
+			count++;
+			break;
+		}
+	}
+	station.unlock();
+	return count;
+}
+
+int main()
+{
+	int A = 0, B = 0, C = 0;
+	cout << "Input travel time A, B, C: ";
+	cin >> A >> B >> C;
+	assert(A > 0 && B > 0 && C > 0);
+	thread train1(TrainA, A);
+	thread train2(TrainB, B);
+	thread train3(TrainC, C);
+
+	int count = 0;
+	while (count != 3)
+	{
+		if (TrainA)
+		{
+			count = Station(count);
+		}
+		if (TrainB)
+		{
+			count = Station(count);
+		}
+		if (TrainC)
+		{
+			count = Station(count);
+		}
+	}
+
+	train1.join();
+	train2.join();
+	train3.join();
+}
---
+++ swim.cpp
@@ -0,0 +1,138 @@
+﻿#include <vector>
+#include <thread>
+#include <string>
+#include <cassert>
+#include <iostream>
+using namespace std;
+
+class Data
+{
+	string name = "unknown";
+	int speed = 0, time = 0;
+
+  public:
+
+	void setName(string value)
+	{
+	  name = value;
+	}
+
+	string getName()
+	{
+		return name;
+	}
+
+	void setSpeed(int value)
+	{
+		speed = value;
+	}
+
+	int getSpeed()
+	{
+		return speed;
+	}
+
+	int getTime()
+	{
+		return time;
+	}
+
+	int calculateDistance(int inTime)
+	{
+		time = inTime;
+		int path = 0;
+		path = time * speed;
+		if (path > 100)
+		{
+			path = 100;
+		}
+		return path;
+	}
+};
+
+class Swim
+{
+	vector<Data>vec;
+	vector<Data>res;
+
+  public:
+	
+	void setVec(Data& data)
+	{
+		vec.push_back(data);
+	}
+
+	vector<Data> getVec()
+	{
+		return vec;
+	}
+
+	void outVec(Data& data, int count)
+	{
+		data = vec.at(count);
+	}
+
+	void outRes(Data& data, int count)
+	{
+		data = res.at(count);
+	}
+
+	void erase(Data& data, int count)
+	{
+		res.push_back(data);
+		vec.erase(vec.begin() + count);
+	}
+};
+
+void Match(Data& data, Swim& swim)
+{
+	int q = 0, time = 1;
+	while (q != -1)
+	{
+		if (swim.getVec().empty())
+		{
+			q = -1;
+			continue;
+		}
+		this_thread::sleep_for(chrono::seconds(1));
+		cout << time << " second" << endl;
+		for (int i = 0; i < swim.getVec().size(); ++i)
+		{
+			swim.outVec(data, i);
+			cout << "Traversed path " << data.getName() << ": " << data.calculateDistance(time) << " meters" << endl;
+			if (data.calculateDistance(time) == 100)
+			{
+				swim.erase(data, i);
+			}
+		}
+		time++;
+	}
+}
+
+int main()
+{
+	string name;
+	int speed = 0;
+	Swim* swim = new Swim();
+	Data* data = new Data();
+
+	for (int j = 1; j <= 5; ++j)
+	{
+		cout << "Input name  " << j << " swimmer: ";
+		cin >> name;
+		cout << "Input speed " << j << " swimmer: ";
+		cin >> speed;
+		data->setName(name);
+		data->setSpeed(speed);
+		swim->setVec(*data);
+	}	
+	assert(speed > 0 && speed <= 10);
+	cout << endl;
+	Match(*data, *swim);
+	cout << endl;
+	for (int i = 0; i < 5; ++i)
+	{
+		swim->outRes(*data, i);
+		cout << "Swimmer " << data->getName() << ": " << data->getTime() << " seconds" << endl;
+	}
+}
